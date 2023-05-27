d3c9409d632976d422017ca6180709f428327544
HanilJain <shirshjain0111@gmail.com>
2023-05-27 17:47:21 +0530

    keepalive

--- QuizMe/settings.py
+++ QuizMe/settings.py
@@ -117,6 +117,15 @@ DATABASES = {
     }
 }  """
 
+
+keepalive_kwargs = {
+  "keepalives": 1,
+  "keepalives_idle": 60,
+  "keepalives_interval": 10,
+  "keepalives_count": 5
+}
+
+
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
